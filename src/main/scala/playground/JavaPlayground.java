b017fd1dfcfe4afb7d9f0c1533aab6e9549d59f7
Cristian David Chavez Quintero <cristian.chavez@jikkosoft.com>
2022-04-14 11:53:48 -0500

    first course commit

---
+++ src/main/scala/playground/JavaPlayground.java
@@ -0,0 +1,13 @@
+package playground;
+
+
+
+public class JavaPlayground
+{
+    public static void main(String[] args)
+    {
+        System.out.println("Hello, Java");
+
+
+    }
+}
---
+++ src/main/scala/playground/ScalaPlayground.scala
@@ -0,0 +1,6 @@
+package playground
+
+object ScalaPlayground extends  App
+{
+  println("Hello from the advanced scala course, this will be a wild ride !!!")
+}
