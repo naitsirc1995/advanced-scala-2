958bd9832b71a7670d8736772936ea38c3246580
Cristian David Chavez Quintero <cristian.chavez@jikkosoft.com>
2022-04-15 10:55:32 -0500

    First branch commit

---
+++ src/main/scala/lectures/part1as/Recap.scala
@@ -0,0 +1,6 @@
+package lectures.part1as
+
+object Recap extends App
+{
+  println("Do not ever underestimate anything, so, pay attention !!")
+}
--- src/main/scala/playground/JavaPlayground.java
+++ src/main/scala/playground/JavaPlayground.java
@@ -7,7 +7,5 @@ public class JavaPlayground
     public static void main(String[] args)
     {
         System.out.println("Hello, Java");
-
-
     }
 }
